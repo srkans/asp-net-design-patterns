d934bec787e6e50b0d1a7579fe80275ede68a51b
srkans <serkansacma@hotmail.com>
2023-01-03 13:28:56 +0300

    invoker class implemented

---
+++ base-project/WebAppCommand/Commands/FileCreateInvoker.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCommand.Commands
+{
+    public class FileCreateInvoker //Invoker
+    {
+        private ITableActionCommand _tableActionCommand;
+        private List<ITableActionCommand> tableActionCommands = new List<ITableActionCommand>();
+
+        public void SetCommand(ITableActionCommand tableActionCommand)
+        {
+            _tableActionCommand = tableActionCommand;
+        }
+
+        public void AddCommand(ITableActionCommand tableActionCommand)
+        {
+            tableActionCommands.Add(tableActionCommand);    
+        }
+
+        public IActionResult CreateFile()
+        {
+            return _tableActionCommand.Execute();
+        }
+
+        public List<IActionResult> CreateFiles()
+        {
+            var list = new List<IActionResult>();
+
+            return tableActionCommands.Select(x => x.Execute()).ToList(); 
+        }
+    }
+}
