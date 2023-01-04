f5cb5ca1f0874971e8b6e88013a2c58c90cf17ac
srkans <serkansacma@hotmail.com>
2023-01-04 10:31:22 +0300

    email class

--- base-project/WebAppObserver/Observer/UserObserverSendEmail.cs
+++ base-project/WebAppObserver/Observer/UserObserverSendEmail.cs
@@ -1,4 +1,6 @@
-﻿using WebAppObserver.Models;
+﻿using System.Net;
+using System.Net.Mail;
+using WebAppObserver.Models;
 
 namespace WebAppObserver.Observer
 {
@@ -14,6 +16,26 @@ namespace WebAppObserver.Observer
         public void CreateUser(AppUser appUser)
         {
            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverSendEmail>>();
+
+            var mailMessage = new MailMessage();
+
+            var smtpClient = new SmtpClient("smtp.gmail.com");
+
+            mailMessage.From = new MailAddress("serkansacma@gmail.com");
+
+            mailMessage.To.Add(new MailAddress(appUser.Email));
+
+            mailMessage.Subject = "Mail Deneme";
+            mailMessage.Body = "<p>Html formatında güzel bir mesaj yazılabilir</p>";
+            mailMessage.IsBodyHtml= true;
+
+            smtpClient.Port = 587;
+
+            smtpClient.Credentials = new NetworkCredential("serkansacma@gmail.com", "password");
+
+            smtpClient.Send(mailMessage);
+
+            logger.LogInformation($"Email has been sent to user {appUser.UserName}");
         }
     }
 }
