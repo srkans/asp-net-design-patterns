c9dd3d84988c0203b68d364dab650606d610bfca
srkans <serkansacma@hotmail.com>
2023-01-04 19:36:07 +0300

    zipFileProcessHandler

---
+++ base-project/WebAppChain/ChainOfResponsibility/ZipFileProcessHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Compression;
+
+namespace WebAppChain.ChainOfResponsibility
+{
+    public class ZipFileProcessHandler<T> : ProcessHandler
+    {
+        public override object Handle(object o)
+        {
+            var excelMS = o as MemoryStream;
+
+            excelMS.Position = 0; // icerisinde byte array tutuyor yazdirmadan once baslangic indeksini 0 olarak ayarlamak gerekiyor.
+
+            using (var zipStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(zipStream,ZipArchiveMode.Create))
+                {
+                    var zipFile = archive.CreateEntry($"{typeof(T).Name}.xlsx");
+
+                    using(var zipEntry = zipFile.Open())
+                    {
+                        excelMS.CopyTo(zipEntry);   
+                    }
+                }
+
+                return base.Handle(zipStream);
+            }
+
+           
+        }
+    }
+}
