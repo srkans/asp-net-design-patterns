14edb16f1ae2e1c1ba9a915b52ab9bb965bb246e
srkans <serkansacma@hotmail.com>
2023-01-06 14:17:16 +0300

    adaptee class added to project

---
+++ base-project/WebAppAdapter/Services/AdvanceImageProcess.cs
@@ -0,0 +1,28 @@
+﻿using LazZiya.ImageResize;
+using System.Drawing;
+
+namespace WebAppAdapter.Services
+{
+    public class AdvanceImageProcess : IAdvanceImageProcess
+    {
+        public void AddWatermarkImage(Stream stream, string text, string filePath, Color color, Color outlineColor)
+        {
+            using (var img = Image.FromFile(@"wwwroot\images\image-file.jpg"))
+            {
+                var tOps = new TextWatermarkOptions
+                {
+                    // Change text color and opacity
+                    // Text opacity range depends on Color's alpha channel (0 - 255)
+                    TextColor = color,
+
+                    // Add text outline
+                    // Outline color opacity range depends on Color's alpha channel (0 - 255)
+                    OutlineColor = outlineColor
+                };
+
+                img.AddTextWatermark(text, tOps)
+                   .SaveAs(filePath);
+            }
+        }
+    }
+}
---
+++ base-project/WebAppAdapter/Services/IAdvanceImageProcess.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace WebAppAdapter.Services
+{
+    public interface IAdvanceImageProcess
+    {
+        //void AddWatermark(string text, string filename, Stream imageStream);
+
+        void AddWatermarkImage(Stream stream, string text, string filePath, Color color, Color outlineColor);
+    }
+}
